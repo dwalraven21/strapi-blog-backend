764481326269c6a4c0c1c4e033ab6a2dc8e11ed6
Danielle Walraven <dwalraven21@gmail.com>
2020-09-11 17:06:36 -0400

    Fixed admin panel impossible to open in browser

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://shielded-headland-61262.herokuapp.com/',
+  url: 'https://shielded-headland-61262.herokuapp.com',
   admin: {
     url: '/',
     serveAdminPanel: false
