726ba05b42e66d11548476cae5f5598ab054cf02
Danielle Walraven <dwalraven21@gmail.com>
2020-09-11 12:57:27 -0400

    Updated DB

--- config/database.js
+++ config/database.js
@@ -2,10 +2,9 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        filename: env('DATABASE_URI'),
       },
       options: {
         useNullAsDefault: true,
